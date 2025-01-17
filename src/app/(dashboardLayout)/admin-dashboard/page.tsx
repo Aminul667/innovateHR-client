498e6c9e1f7b3d75ddd952a1c6c23c8066ed7e91
aminul <rahathssn@gmail.com>
2025-01-17 21:43:53 +0600

    added sidebar and page skeletion

--- src/app/(dashboardLayout)/admin-dashboard/page.tsx
+++ src/app/(dashboardLayout)/admin-dashboard/page.tsx
@@ -1,11 +1,50 @@
-import IHSidebar from "@/app/_components/UI/Sidebar/IHSidebar";
+// import IHSidebar from "@/app/_components/UI/Sidebar/IHSidebar";
 import React from "react";
+import { AppSidebar } from "../components/adminDashboardNavbar/app-sidebar";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 const page = () => {
   return (
-    <div>
-      <IHSidebar />
-      <h2>This is admin dashboard page</h2>
+    <div className="flex w-full">
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4 bg-red-600">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50 bg-slate-500" />
+            <div className="aspect-video rounded-xl bg-muted/50 bg-slate-500" />
+            <div className="aspect-video rounded-xl bg-muted/50 bg-slate-500" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min bg-slate-500" />
+        </div>
+      </SidebarInset>
     </div>
   );
 };
